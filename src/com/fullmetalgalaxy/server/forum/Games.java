aaf8b7400b9b2f4e6f470d2052be1a277b67025b
Unknown <vincent.legendre@gmail.com>
2011-09-19 14:17:54 +0200

    try to post new game as subject on forum and get them from rss

---
+++ src/com/fullmetalgalaxy/server/forum/Games.java
@@ -0,0 +1,183 @@
+/* *********************************************************************
+ *
+ *  This file is part of Full Metal Galaxy.
+ *  http://www.fullmetalgalaxy.com
+ *
+ *  Full Metal Galaxy is free software: you can redistribute it and/or 
+ *  modify it under the terms of the GNU Affero General Public License
+ *  as published by the Free Software Foundation, either version 3 of 
+ *  the License, or (at your option) any later version.
+ *
+ *  Full Metal Galaxy is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public 
+ *  License along with Full Metal Galaxy.  
+ *  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Copyright 2010, 2011 Vincent Legendre
+ *
+ * *********************************************************************/
+
+package com.fullmetalgalaxy.server.forum;
+
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.jdom.Element;
+import org.jdom.input.SAXBuilder;
+
+import com.fullmetalgalaxy.model.ressources.SharedI18n;
+import com.fullmetalgalaxy.server.ServerUtil;
+import com.google.appengine.api.memcache.Expiration;
+import com.google.appengine.api.memcache.MemcacheService;
+import com.google.appengine.api.memcache.MemcacheServiceFactory;
+
+/**
+ * create an html text from an RSS stream.
+ * 
+ * @author vlegendr
+ *
+ */
+public class Games
+{
+  private static final int GAMES_ITEM_COUNT = 3;
+  private static final String CACHE_NEWS_KEY = "CACHE_GAMES_KEY";
+  private static final int CACHE_NEWS_TTL_SEC = 3600; // one hour
+  
+  private static final DateFormat s_pubDateFormat = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz", Locale.ENGLISH);
+
+  // regex pattern that match game url
+  private static Pattern s_gameUrlPattern = Pattern.compile(
+      ".*href=\"http://www.fullmetalgalaxy.com/game.jsp\\?id=([^\"]*)\".*", Pattern.DOTALL );
+  
+    
+  
+  /**
+   * 
+   * @return news in an HTML format ready to be included in JSP pages.
+   */
+  public static String getHtml()
+  {
+    String newsHtml = String.class.cast( getCache().get( CACHE_NEWS_KEY ) );
+    if( newsHtml == null )
+    {
+      newsHtml = buildNewsHtml();
+      getCache().put( CACHE_NEWS_KEY, newsHtml, Expiration.byDeltaSeconds( CACHE_NEWS_TTL_SEC ) );
+    }
+    
+    return newsHtml;
+  }
+  
+  
+  /**
+   * build news from RSS to an HTML format ready to be included in JSP pages.
+   * @return 
+   */
+  private static String buildNewsHtml()
+  {
+    String newsHtml = "";
+    
+    int itemCount = 0;
+    DateFormat dateFormat = new SimpleDateFormat( SharedI18n.getMisc( 0 ).dateFormat() );
+    for( Element item : getNewsItem() )
+    {
+      // for each item:
+      // read relevent informations
+      // title, link, description, pubDate
+      String title = item.getChild("title").getText();
+      String link = item.getChild("link").getText();
+      String description = item.getChild("description").getText();
+      Date pubDate = new Date();
+      try
+      {
+        pubDate = s_pubDateFormat.parse(item.getChild("pubDate").getText());
+      } catch( ParseException e )
+      {
+        e.printStackTrace();
+      }
+
+      // find game url in message description
+      Matcher matcher = s_gameUrlPattern.matcher( description );
+      if( matcher.matches() )
+      {
+        String gameId = matcher.group( 1 );
+        link = "/game.jsp?id=" + gameId;
+      }
+
+
+      // add a news entry
+      newsHtml += "<a href='" + link + "'><div class='article'><article><span class='date'>"
+ + dateFormat.format( pubDate )
+   // <h4> tag cause graphic glich on IE7
+          + "</span><div class='h4'>" + title + "</div></article></div></a>";
+        
+      // and stop if we've got enough
+      itemCount++;
+      if( itemCount >= GAMES_ITEM_COUNT)
+      {
+        break;
+      }
+    }
+    
+    return newsHtml;
+  }
+  
+
+  @SuppressWarnings("unchecked")
+  private static List<Element> getNewsItem()
+  {
+    List<Element> listItem = new ArrayList<Element>();
+      
+    org.jdom.Document document;
+    // On crée une instance de SAXBuilder
+    SAXBuilder sxb = new SAXBuilder();
+    try
+    {
+      URL url = new URL( ServerUtil.newsConnector().getNewsRssUrl(
+          ConectorImpl.FORUM_GAMES_THREAD_ID ) );
+      // On crée un nouveau document JDOM avec en argument le fichier XML
+      // Le parsing est terminé ;)
+       document = sxb.build( url.openStream() );
+  
+      // On initialise un nouvel élément racine avec l'élément racine du
+      // document.
+      Element racine = document.getRootElement();
+  
+      // On crée une List contenant tous les noeuds "etudiant" de l'Element
+      // racine
+      listItem = racine.getChild("channel").getChildren("item");
+    }
+    catch(Exception e)
+    {
+      e.printStackTrace();
+    }
+    return listItem;
+  }
+  
+  private static MemcacheService s_cache = null;
+
+  /**
+   * @return the s_cache
+   */
+  private static MemcacheService getCache()
+  {
+    if( s_cache == null )
+    {
+      s_cache = MemcacheServiceFactory.getMemcacheService();
+    }
+    return s_cache;
+  }
+
+
+}
