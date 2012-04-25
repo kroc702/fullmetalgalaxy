e7c297685b6800ad9eeb7bdee6f3b70b2d9d1237
Unknown <vincent.legendre@gmail.com>
2012-04-25 22:08:38 +0200

    update copyright year

--- src/com/fullmetalgalaxy/server/xmpp/RessourceGraphStorage.java
+++ src/com/fullmetalgalaxy/server/xmpp/RessourceGraphStorage.java
@@ -1,22 +1,25 @@
-/*
-  net.charabia.generation java package : Random text generation package
-  Copyright (C) 1999 Rodrigo Reyes, reyes@chez.com, reyes@linuxbox.com
-
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-
-*/
+/* *********************************************************************
+ *
+ *  This file is part of Full Metal Galaxy.
+ *  http://www.fullmetalgalaxy.com
+ *
+ *  Full Metal Galaxy is free software: you can redistribute it and/or 
+ *  modify it under the terms of the GNU Affero General Public License
+ *  as published by the Free Software Foundation, either version 3 of 
+ *  the License, or (at your option) any later version.
+ *
+ *  Full Metal Galaxy is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public 
+ *  License along with Full Metal Galaxy.  
+ *  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Copyright 2010, 2011, 2012 Vincent Legendre
+ *
+ * *********************************************************************/
 package com.fullmetalgalaxy.server.xmpp;
 
 import java.io.IOException;
