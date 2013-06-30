b05c8968297774a9b0942ef47391e1e4fc504907
vincent.legendre@gmail.com <vincent.legendre@gmail.com>
2013-07-01 00:07:58 +0200

    add new 3D model: destroyer

--- src/com/fullmetalgalaxy/client/ressources/tokens/render.bat
+++ src/com/fullmetalgalaxy/client/ressources/tokens/render.bat
@@ -16,6 +16,7 @@ blender -b walkertank.blend -P script.py
 blender -b tarask.blend -P script.py
 blender -b hovertank.blend -P script.py
 blender -b crayfish.blend -P script.py
+blender -b destroyer.blend -P script.py
 
 rem find . -path ./render/*.png -exec convert \{\} -sharpen 0x.7 \{\} \;
 
