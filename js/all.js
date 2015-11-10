f99c1ee838566ac65f84f3688f70020ea52ccd2f
kroc702 <vincent.legendre@gmail.com>
2015-11-10 23:51:56 +0100

    remove js function to edit content

--- js/all.js
+++ js/all.js
@@ -2,6 +2,4 @@ function goToProse(repo, page) {
     window.location = repo.replace(/^https?:\/\/[^\/]*\//i,'http://prose.io/#') + '/edit/gh-pages/' + page;
 }
 
-function goToGitHub(repo, page) {
-    window.location = repo + '/edit/gh-pages/' + page;
-}
+
